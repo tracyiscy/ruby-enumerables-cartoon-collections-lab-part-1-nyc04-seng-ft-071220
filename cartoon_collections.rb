e3506abeef8fdb028677b62f73d16c1ecf7f4b14
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-21 10:35:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,4 +9,8 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index{|name,index|
+    puts "#{index+1}. #{name}"
+  }
+
 end
